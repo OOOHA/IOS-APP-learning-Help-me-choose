139cc5031603c9c0e9dab4b4fd75f487becab8f2
OOOHA <45551120+OOOHA@users.noreply.github.com>
2021-07-15 22:40:20 +0800

    Initial Commit

---
+++ Help me choose/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Help me choose
+//
+//  Created by 邱宏宇 on 7/15/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Help me choose/Help_me_chooseApp.swift
@@ -0,0 +1,17 @@
+//
+//  Help_me_chooseApp.swift
+//  Help me choose
+//
+//  Created by 邱宏宇 on 7/15/21.
+//
+
+import SwiftUI
+
+@main
+struct Help_me_chooseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
